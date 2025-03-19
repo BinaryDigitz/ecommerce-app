6ccd2a8a7b05a8365f20e9b1763413bd4b324c43
Binary Digitz <borisndoh@gmail.com>
2025-03-19 21:53:58 +0100

    Add related product

--- frontend/src/components/DisplayRelatedProduct.jsx
+++ frontend/src/components/DisplayRelatedProduct.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { ShopContext } from './exportComp'
+import { ProductCart, ShopContext, Title } from './exportComp'
 
 function DisplayRelatedProduct({ category, subcategory }) {
    const { products } = useContext(ShopContext);
@@ -10,15 +10,23 @@ if (products.length > 0) {
     let productCopy = products.slice();
     productCopy = productCopy.filter(item => category === item.category)
     productCopy = productCopy.filter(item =>subcategory === item.subCategory)
+    setRelatedProduct(productCopy.slice(0, 5))
 
-    console.log(productCopy.slice(0, 5));
     
 }
    },[products])
   return (
     <div className='my-24 '>
-      <div className='text-center text'>
-
+      <div className='text-center text-3xl py-2'>
+        <Title text1={'RELATED'} text2={'PRODUCTS'}/>
+      </div>
+      <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 gap-y-6'>
+{
+relatedProduct.map((item, index) => (
+    
+    <ProductCart key={index} product={item} />
+))
+}
       </div>
     </div>
   )
